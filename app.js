c76e71b0f7e5039677fcb69eabc3dc868300acf2
JeyBySy <njcocosa@gmail.com>
2025-01-10 03:08:07 +0800

    Add product routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { json, urlencoded } = require('express');
 // const authRoutes = require('./routes/authRoutes');
 // const userRoutes = require('./routes/userRoutes');
 // const errorHandler = require('./middleware/errorHandler');
+const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(urlencoded({ extended: true })); // Parse URL-encoded payloads
 // Routes
 // app.use('/api/auth', authRoutes);
 // app.use('/api/users', userRoutes);
+app.use('/api/product', productRoutes);
 
 app.get("/", (req, res) => {
     res.send({ status: "dasdasdasds" })
